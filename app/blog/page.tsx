e374b7c1c759e9327ba53f1af5bb4c0d02d9f704
Amit Raj <amitraj@Amits-MacBook-Pro.local>
2025-04-23 21:05:40 +0530

    Version 1 of my website

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Blog</h1>
+      <p className="text-gray-700">
+        Welcome to my blog! Here I share my thoughts on various topics.
+      </p>
+      {/* Add blog post listings here later */}
+    </div>
+  );
+} 
